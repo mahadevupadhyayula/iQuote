d07ea51d2f1e5976b8deea987ae244ab3724de32
Mahadev Upadhyayula <mahadevu318@gmail.com>
2026-07-19 12:21:23 +0530

    Fix ESLint config ESM imports

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig, globalIgnores } from 'eslint/config';
-import nextVitals from 'eslint-config-next/core-web-vitals';
-import nextTypescript from 'eslint-config-next/typescript';
+import nextVitals from 'eslint-config-next/core-web-vitals.js';
+import nextTypescript from 'eslint-config-next/typescript.js';
 import prettier from 'eslint-config-prettier/flat';
 
 export default defineConfig([
