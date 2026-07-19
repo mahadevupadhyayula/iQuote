94a939f9bbb4fffccbecdd6be2eceb4a0703c0c8
Mahadev Upadhyayula <mahadevu318@gmail.com>
2026-07-19 13:23:06 +0530

    Wrap server action exports for build

--- app/quotes/[quoteId]/actions.ts
+++ app/quotes/[quoteId]/actions.ts
@@ -1,13 +1,60 @@
 "use server";
 
-export {
-  applyRepCorrections,
-  continueQuoteConfiguration,
-  createQuoteDraft,
-  extractAndBuildQuote,
-  generateQuote,
-  saveQuoteDraft,
-  selectFulfillment,
-  sendQuote,
-  submitQuoteForApproval,
+import {
+  applyRepCorrections as applyRepCorrectionsAction,
+  continueQuoteConfiguration as continueQuoteConfigurationAction,
+  createQuoteDraft as createQuoteDraftAction,
+  extractAndBuildQuote as extractAndBuildQuoteAction,
+  generateQuote as generateQuoteAction,
+  saveQuoteDraft as saveQuoteDraftAction,
+  selectFulfillment as selectFulfillmentAction,
+  sendQuote as sendQuoteAction,
+  submitQuoteForApproval as submitQuoteForApprovalAction,
 } from "@/lib/actions/quote-actions";
+import type {
+  ApplyRepCorrectionsActionInput,
+  ContinueQuoteConfigurationActionInput,
+  CreateQuoteDraftActionInput,
+  ExtractAndBuildQuoteActionInput,
+  GenerateQuoteActionInput,
+  SaveQuoteDraftActionInput,
+  SelectFulfillmentActionInput,
+  SendQuoteActionInput,
+  SubmitQuoteForApprovalActionInput,
+} from "@/lib/schemas/quote-action-schemas";
+
+export async function applyRepCorrections(input: ApplyRepCorrectionsActionInput) {
+  return applyRepCorrectionsAction(input);
+}
+
+export async function continueQuoteConfiguration(input: ContinueQuoteConfigurationActionInput) {
+  return continueQuoteConfigurationAction(input);
+}
+
+export async function createQuoteDraft(input: CreateQuoteDraftActionInput) {
+  return createQuoteDraftAction(input);
+}
+
+export async function extractAndBuildQuote(input: ExtractAndBuildQuoteActionInput) {
+  return extractAndBuildQuoteAction(input);
+}
+
+export async function generateQuote(input: GenerateQuoteActionInput) {
+  return generateQuoteAction(input);
+}
+
+export async function saveQuoteDraft(input: SaveQuoteDraftActionInput) {
+  return saveQuoteDraftAction(input);
+}
+
+export async function selectFulfillment(input: SelectFulfillmentActionInput) {
+  return selectFulfillmentAction(input);
+}
+
+export async function sendQuote(input: SendQuoteActionInput) {
+  return sendQuoteAction(input);
+}
+
+export async function submitQuoteForApproval(input: SubmitQuoteForApprovalActionInput) {
+  return submitQuoteForApprovalAction(input);
+}
